d78667680e903c0458984ed82fbd2752fe00cc84
AlliiAbbas <116305973+AlliiAbbas@users.noreply.github.com>
2025-09-28 00:59:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -154,12 +154,12 @@ st.markdown("""
         background-color: #161616 !important; 
         border: none !important; 
     }
-    .stChatInputContainer  {
+    .st-emotion-cache-x1bvup  {
         background: #38383880 !important;
         border: 2px solid var(--light-dark-1004, #FFFFFF0A);
         background: linear-gradient(105.13deg, #1C1C1C 41.52%, rgba(63, 63, 63, 0) 100%);
     }
-    .stTextInput textarea:focus, .stChatInputContainer textarea:focus{
+    .stTextInput textarea:focus, .st-emotion-cache-x1bvup textarea:focus{
         box-shadow: 0px 0.84px 0px 0px #FFFFFF14 inset;
         box-shadow: 2px 2px 19px 0px #FFFFFF1A;
         border: 2px solid var(--light-dark-1004, #FFFFFF0A);
