4b84270eab78b304b7fa6868868c07af9851629b
AlliiAbbas <116305973+AlliiAbbas@users.noreply.github.com>
2025-09-28 01:59:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,13 @@ st.markdown("""
     .st-emotion-cache-liupih{
         padding-bottom: 25px;
     }
-    
+    .st-emotion-cache-19cfm8f{
+       background: linear-gradient(105.13deg, #1C1C1C 41.52%, rgba(63, 63, 63, 0) 100%);
+        color: white; 
+        border: 2px solid var(--light-dark-1004, #FFFFFF0A);
+        border-radius: 16px; 
+        padding: 12px; 
+    }
     </style>
     """, unsafe_allow_html=True)
 
