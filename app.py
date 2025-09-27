eac6ff4ffa6fc23a6eb8f011e626c52c56311fc2
AlliiAbbas <116305973+AlliiAbbas@users.noreply.github.com>
2025-09-28 01:10:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -143,8 +143,8 @@ st.markdown("""
     div[data-baseweb="notification"] {
         background: linear-gradient(105.13deg, #1C1C1C 41.52%, rgba(63, 63, 63, 0) 100%);
         color: white; 
-        border: 0.84px solid var(--light-dark-1004, #FFFFFF0A);
-        border-radius: 12px; 
+        border: 2px solid var(--light-dark-1004, #FFFFFF0A);
+        border-radius: 16px; 
         padding: 12px; 
     }
     .st-emotion-cache-hzygls  {
