463a2517a400d3721140855211fced75928ef8ec
AlliiAbbas <116305973+AlliiAbbas@users.noreply.github.com>
2025-09-28 00:40:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -159,11 +159,12 @@ st.markdown("""
         background: #38383880 !important;
         border: 2px solid var(--light-dark-1004, #FFFFFF0A);
         background: linear-gradient(105.13deg, #1C1C1C 41.52%, rgba(63, 63, 63, 0) 100%);
-        height:100px;
 
     }
     .st-emotion-cache-x1bvup textarea {
             background-color: transparent;
+                    height:100px;
+
     }
     .st-emotion-cache-x1bvup .st-f9{
     background-color:transparent;
