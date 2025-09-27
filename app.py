43facb10f29b6d7196d0dfeced19df1d5a9a86ad
AlliiAbbas <116305973+AlliiAbbas@users.noreply.github.com>
2025-09-27 23:38:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ st.markdown("""
      header[data-testid="stHeader"] {
         background-color: #38383880;
     }
-     div[data-baseweb="alert"] {
+     div[data-baseweb="stAlert"] {
         background: linear-gradient(105.13deg, #1C1C1C 41.52%, rgba(63, 63, 63, 0) 100%);
         color: white; 
         border: none; 
