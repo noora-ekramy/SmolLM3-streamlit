392713b04416e3d80257a30194164c0ecc4c5765
AlliiAbbas <116305973+AlliiAbbas@users.noreply.github.com>
2025-09-28 03:50:41 +0300

    Update cost_calculator.py

--- cost_calculator.py
+++ cost_calculator.py
@@ -92,6 +92,9 @@ def cost_calculator_page():
     .st-emotion-cache-55l0h8{
        background:black
     }
+    .st-emotion-cache-1hiktyo:hover{
+      linear-gradient(105.13deg, #292929 41.52% 41.52% , rgba(63, 63, 63, 0) 100%) !important
+    }
     </style>
     """, unsafe_allow_html=True)
     
